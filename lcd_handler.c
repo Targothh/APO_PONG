a51a7d738817257a90199cdf6b3b56243ccf898b
sedlaon8 <sedlaon8@fel.cvut.cz>
2024-05-16 15:21:13 +0200

    add: font support

--- lcd_handler.c
+++ lcd_handler.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
-#include "mzapo_parlcd.h"
-#include "font_types.h"
-#include "mzapo_phys.h"
-#include "mzapo_regs.h"
+#include "lcd_handler.h"
 #define BLACK   0x0000
 #define WHITE   0xFFFF
 #define RED     0xF800
@@ -29,8 +25,41 @@ void draw(uint16_t * buffer, unsigned char *parlcd_mem_base){
     }
 }
 
-// void draw_text(uint16_t * buffer, font_descriptor_t *font, int x, int y, int char_code, int color){
-    
+int char_width(int ch, font_descriptor_t *font) {
+  int width;
+  if (!font->width) {
+    width = font->maxwidth;
+  } else {
+    width = font->width[ch - font->firstchar];
+  }
+  return width;
+}
+ 
+void draw_char(int x, int y, char ch, unsigned short color) {
+  
+}
+void draw_text(uint16_t * buffer, font_descriptor_t *font, int x, int y, int scale,  int char_code, int color){
+    int w = char_width(char_code, font);
+  const font_bits_t *ptr;
+  if ((char_code >= font->firstchar) && (char_code-font->firstchar < font->size)) {
+    if (font->offset) {
+      ptr = &font->bits[font->offset[char_code-font->firstchar]];
+    } else {
+      int bw = (font->maxwidth+15)/16;
+      ptr = &font->bits[(char_code - font->firstchar)*bw*font->height];
+    }
+    int i, j;
+    for (i=0; i<font->height; i++) {
+      font_bits_t val = *ptr;
+      for (j=0; j<w; j++) {
+        if ((val&0x8000)!=0) {
+          draw_pixel(buffer ,x+scale*j, y+scale*i, color);
+        }
+        val<<=1;
+      }
+      ptr++;
+    }
+  }
     
 
-// }
+}
--- lcd_handler.h
+++ lcd_handler.h
@@ -1,5 +1,10 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include "mzapo_parlcd.h"
+#include "font_types.h"
+#include "mzapo_phys.h"
+#include "mzapo_regs.h"
 uint16_t * buffer_init();
 void draw_pixel(uint16_t * buffer, int x, int y, uint16_t data);
 void draw(uint16_t *buffer, unsigned char *parlcd_mem_base);
-
+void draw_text(uint16_t * buffer, font_descriptor_t *font, int x, int y, int scale, int char_code, int color);
